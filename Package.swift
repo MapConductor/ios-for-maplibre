45761bb5c19f89a5e806427d16394c5af9decf2b
Masashi Katsumata <wf9a5m75@gmail.com>
2026-01-14 16:04:59 +0900

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "mapconductor-for-maplibre",
+    platforms: [
+        .iOS(.v15),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MapConductorForMapLibre",
             targets: ["MapConductorForMapLibre"]
@@ -17,14 +17,12 @@ let package = Package(
         .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", from: "6.20.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MapConductorForMapLibre",
             dependencies: [
                 .product(name: "MapConductorCore", package: "ios-sdk-core"),
-                .product(name: "MapLibre Native", package: "maplibre-gl-native-distribution"),
-            ],
+                .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
+            ]
         ),
     ]
 )
